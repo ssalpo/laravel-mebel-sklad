1322b8f6177b65b1229a2c2896cdc349364df0e5
ssalpo <ssalpomishevich@gmail.com>
2023-05-22 08:33:35 +0500

    поправил поиск по баркоду

--- database/migrations/2023_05_15_142215_add_barcode_column_to_nomenclatures_table.php
+++ database/migrations/2023_05_15_142215_add_barcode_column_to_nomenclatures_table.php
@@ -4,15 +4,14 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration
-{
+return new class extends Migration {
     /**
      * Run the migrations.
      */
     public function up(): void
     {
         Schema::table('nomenclatures', function (Blueprint $table) {
-            $table->bigInteger('barcode')->nullable();
+            $table->string('barcode')->nullable();
         });
     }
 
